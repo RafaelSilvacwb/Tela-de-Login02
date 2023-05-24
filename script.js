7168b968f20674ab2a5adf189d3807c89d08d598
RAFAEL ASSIS DA SILVA <123268089+RafaelSilvacwb@users.noreply.github.com>
2023-05-24 20:35:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ function goBack() {
 function goFront() {
     document.querySelector("#box")
         .style.transform = "rotateY(0deg)"
+        event.preventDefault()
+}
+
+function recovery() {
+    document.querySelector("#recover")
+    .style.transform = "rotateY(0deg)"
+    document.getElementById("box")
+    .style.display = "none"
+    
+    event.preventDefault()
 }
 
 
